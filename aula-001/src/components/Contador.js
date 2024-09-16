7fddbdef2de79f2d4b0fdde2eee3c3ecc72ffa20
Kawan Wagnner <kawanwagnner7@gmail.com>
2024-09-16 16:42:52 -0300

    new changes

--- aula-001/src/components/Contador.js
+++ aula-001/src/components/Contador.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
 export default function Contador() {
+  const [count, setCount] = useState(0);
+  const [msg, setMsg] = useState(0);
+
+  useEffect(() => {
+    setMsg("O valor do conunt foi atualizado " + count + " Vez(es)");
+  }, [count]);
+
   return (
     <>
-      <Text style={styles.texto}>Você clicou aqui</Text>
+      <Text style={styles.texto}>Clique para adicionar = {count}</Text>
+      <Text style={styles.msg}>{msg}</Text>
 
       <View style={styles.buttonList}>
-        <Button title="Clique aqui" />
+        <Button
+          onPress={() => {
+            setCount(count + 1);
+          }}
+          title="Clique aqui"
+        />
       </View>
 
       <View style={styles.buttonList}>
-        <Button title="Diminuir" />
+        <Button
+          onPress={() => {
+            if (!(count <= 0)) {
+              setCount(count - 1);
+            } else {
+              alert("Minímo excedido");
+            }
+          }}
+          title="Diminuir"
+        />
       </View>
     </>
   );
@@ -20,6 +42,10 @@ export default function Contador() {
 const styles = StyleSheet.create({
   buttonList: {
     padding: 10,
+  },
+
+  msg: {
+    textAlign: "center",
   },
 
   texto: {
